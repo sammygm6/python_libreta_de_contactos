842836213e17539fbc64939e4f8f3bb25adec79b
Sammy <sammygm6@gmail.com>
2017-08-06 20:42:26 -0600

    subiendo website

---
+++ contactos/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class contacto(models.Model):
+    identificador = models.CharField(max_length = 50)
+    nombre = models.CharField(max_length = 50)
+    telefono = models.CharField(max_length = 50)
+    email = models.CharField(max_length = 50)
+    referencias = models.CharField(max_length = 50)
+    vinculo = models.CharField(max_length = 50)
